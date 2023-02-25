094ce4b411307f52bc29cfa1b4654c7473587ec8
rp-bot_win10 <legacy.business18@gmail.com>
2023-02-25 09:12:37 -0500

    -p

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -4,21 +4,18 @@ import { KEYS, OCTAVE_STRUCTURE, OCTAVE_RANGES } from "../constants/constants";
 
 const Piano = () => {
 	return (
-    <div className=" grid grid-cols-1 w-1 h-0">
-      {OCTAVE_RANGES.map((octave) => (
+		<div className=" grid grid-cols-1 w-1 h-0">
+			{OCTAVE_RANGES.map((octave) =>
 				OCTAVE_STRUCTURE.map((key, index) => (
-          <Key key={index} keyColor={KEYS[key]} midiNote={octave[0] + index} />
-<<<<<<< HEAD
-				))
-      ))}
+					<Key
+						key={index}
+						keyColor={KEYS[key]}
+						midiNote={octave[0] + index}
+					/>
+				)).reverse()
+			).reverse()}
 		</div>
 	);
-=======
-        )).reverse()
-      )).reverse()}
-    </div>
-  );
->>>>>>> b196de12c5c02b2ab59739f6af28d0e42cd8d00c
 };
 
 export default Piano;
