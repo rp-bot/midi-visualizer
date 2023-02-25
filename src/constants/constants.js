9189b18f7e3ba2720c4d9dc70306ec4f5076357f
rp-bot_win10 <legacy.business18@gmail.com>
2023-02-25 08:44:43 -0500

    -p

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,16 @@
 const KEYS_RANGE = [21, 108];
 // W B W B W W B W B W B W
 const OCTAVE_1 = [
-  { keyNote: 21, keyColor: 'white' },
-  { keyNote: 22, keyColor: 'black' },
-  { keyNote: 23, keyColor: 'white' },
-  { keyNote: 24, keyColor: 'black' },
-  { keyNote: 25, keyColor: 'white' },
-  { keyNote: 26, keyColor: 'white' },
-  { keyNote: 27, keyColor: 'black' },
-  { keyNote: 28, keyColor: 'white' },
-  { keyNote: 29, keyColor: 'black' },
-  { keyNote: 30, keyColor: 'white' },
-  { keyNote: 31, keyColor: 'black' },
-  { keyNote: 32, keyColor: 'white' },
+  { midiNote: 21, keyColor: 'white' },
+  { midiNote: 22, keyColor: 'black' },
+  { midiNote: 23, keyColor: 'white' },
+  { midiNote: 24, keyColor: 'black' },
+  { midiNote: 25, keyColor: 'white' },
+  { midiNote: 26, keyColor: 'white' },
+  { midiNote: 27, keyColor: 'black' },
+  { midiNote: 28, keyColor: 'white' },
+  { midiNote: 29, keyColor: 'black' },
+  { midiNote: 30, keyColor: 'white' },
+  { midiNote: 31, keyColor: 'black' },
+  { midiNote: 32, keyColor: 'white' },
 ];
