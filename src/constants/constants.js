92928eb14f70135b4f1d72867f73275f87be2b2f
SRUTHI2000 <shisru2000@gmail.com>
2023-02-25 19:13:23 +0530

    octave def

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,16 @@
 const KEYS_RANGE = [21, 108];
 // W B W B W W B W B W B W
 const OCTAVE_1 = [
-  {
-
-  }
+  { keyNote: 21, keyColor: 'white' },
+  { keyNote: 22, keyColor: 'black' },
+  { keyNote: 23, keyColor: 'white' },
+  { keyNote: 24, keyColor: 'black' },
+  { keyNote: 25, keyColor: 'white' },
+  { keyNote: 26, keyColor: 'white' },
+  { keyNote: 27, keyColor: 'black' },
+  { keyNote: 28, keyColor: 'white' },
+  { keyNote: 29, keyColor: 'black' },
+  { keyNote: 30, keyColor: 'white' },
+  { keyNote: 31, keyColor: 'black' },
+  { keyNote: 32, keyColor: 'white' },
 ];
