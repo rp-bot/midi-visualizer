aabccca68353ae4bc00942fdd080f4ff44f2b444
rp-bot_win10 <legacy.business18@gmail.com>
2023-02-25 09:16:20 -0500

    -p

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -3,10 +3,16 @@ import React from "react";
 const Key = ({ keyColor, midiNote }) => {
 	console.log(midiNote);
 	if (keyColor == "white") {
-		return <div className="bg-white border border-black px-12 py-2 text-black">{midiNote}</div>;
+		return (
+			<div className="bg-white border border-black px-12  text-black ">
+				{midiNote}
+			</div>
+		);
 	} else {
 		return (
-			<div className=" bg-black border border-black px-12 py-2 text-white">{midiNote}</div>
+			<div className=" bg-black border border-black px-12  text-white">
+				{midiNote}
+			</div>
 		);
 	}
 };
