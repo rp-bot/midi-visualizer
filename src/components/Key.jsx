142c5e09e1ddc0e60b58d9dd9a085443f0cf2fa1
SRUTHI2000 <shisru2000@gmail.com>
2023-02-26 21:08:45 +0530

    keyboard sound

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Key = ({ keyColor, midiNote }) => {
+const Key = ({ keyColor, midiNote, ...attr }) => {
 	console.log(midiNote);
 	if (keyColor == "white") {
 		return (
-			<div className="bg-white border border-black px-12  text-black ">
+			<div className="bg-white border border-black px-12  text-black " {...attr}>
 				{midiNote}
 			</div>
 		);
 	} else {
 		return (
-			<div className=" bg-black border border-black px-12  text-white">
+			<div className=" bg-black border border-black px-12  text-white" {...attr}>
 				{midiNote}
 			</div>
 		);
--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Key from "./Key";
+import * as Tone from "tone";
 import { KEYS, OCTAVE_STRUCTURE, OCTAVE_RANGES } from "../constants/constants";
 
 const Piano = () => {
-	return (
-		<div className=" grid grid-cols-1 w-fit h-[500px] overflow-scroll">
-			{OCTAVE_RANGES.map((octave) =>
-				OCTAVE_STRUCTURE.map((key, index) => (
-					<Key
-						key={index}
-						keyColor={KEYS[key]}
-						midiNote={octave[0] + index}
-						
-					/>
-				)).reverse()
-			).reverse()}
-		</div>
-	);
+  const synth = useRef(null);
+
+  useEffect(() => {
+    synth.current = new Tone.Synth().toDestination();
+  }, []);
+
+  const onMouseDown = (e, n) => {
+    const note = Tone.Frequency(n, "midi").toNote();
+    const now = Tone.now();
+    // trigger the attack immediately
+    synth?.current?.triggerAttack(note, now);
+  };
+
+  const onMouseUp = () => {
+    synth?.current?.triggerRelease();
+  };
+  return (
+    <div className=" grid grid-cols-1 w-fit h-[500px] overflow-scroll">
+      {OCTAVE_RANGES.map((octave) =>
+        OCTAVE_STRUCTURE.map((key, index) => (
+          <Key
+            key={index}
+            keyColor={KEYS[key]}
+            midiNote={octave[0] + index}
+            onMouseDown={(e) => onMouseDown(e, octave[0] + index)}
+            onMouseUp={(e) => onMouseUp(e, octave[0] + index)}
+
+          />
+        )).reverse()
+      ).reverse()}
+    </div>
+  );
 };
 
 export default Piano;
