92534337faee5b3950fbff3c6d3653b589c396a4
rp-bot_win10 <legacy.business18@gmail.com>
2023-02-26 10:45:44 -0500

    -p

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -4,38 +4,38 @@ import * as Tone from "tone";
 import { KEYS, OCTAVE_STRUCTURE, OCTAVE_RANGES } from "../constants/constants";
 
 const Piano = () => {
-  const synth = useRef(null);
+	const synth = useRef(null);
+	//TODO const changecolor = "gray-700"
 
-  useEffect(() => {
-    synth.current = new Tone.Synth().toDestination();
-  }, []);
+	useEffect(() => {
+		synth.current = new Tone.Synth().toDestination();
+	}, []);
 
-  const onMouseDown = (e, n) => {
-    const note = Tone.Frequency(n, "midi").toNote();
-    const now = Tone.now();
-    // trigger the attack immediately
-    synth?.current?.triggerAttack(note, now);
-  };
+	const onMouseDown = (e, n) => {
+		const note = Tone.Frequency(n, "midi").toNote();
+		const now = Tone.now();
+		// trigger the attack immediately
+		synth?.current?.triggerAttack(note, now);
+	};
 
-  const onMouseUp = () => {
-    synth?.current?.triggerRelease();
-  };
-  return (
-    <div className=" grid grid-cols-1 w-fit h-[500px] overflow-scroll">
-      {OCTAVE_RANGES.map((octave) =>
-        OCTAVE_STRUCTURE.map((key, index) => (
-          <Key
-            key={index}
-            keyColor={KEYS[key]}
-            midiNote={octave[0] + index}
-            onMouseDown={(e) => onMouseDown(e, octave[0] + index)}
-            onMouseUp={(e) => onMouseUp(e, octave[0] + index)}
-
-          />
-        )).reverse()
-      ).reverse()}
-    </div>
-  );
+	const onMouseUp = () => {
+		synth?.current?.triggerRelease();
+	};
+	return (
+		<div className=" grid grid-cols-1 w-fit h-[500px] overflow-scroll">
+			{OCTAVE_RANGES.map((octave) =>
+				OCTAVE_STRUCTURE.map((key, index) => (
+					<Key
+						key={index}
+						keyColor={KEYS[key]}
+						midiNote={octave[0] + index}
+						onMouseDown={(e) => onMouseDown(e, octave[0] + index)}
+						onMouseUp={(e) => onMouseUp(e, octave[0] + index)}
+					/>
+				)).reverse()
+			).reverse()}
+		</div>
+	);
 };
 
 export default Piano;
