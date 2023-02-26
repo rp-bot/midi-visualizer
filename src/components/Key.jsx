a8fa3b3b3af07d5aef0be9b853c89aad66b77d60
rp-bot_win10 <legacy.business18@gmail.com>
2023-02-26 15:37:24 -0500

    piano working condition

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -4,14 +4,14 @@ const Key = ({ keyColor, midiNote, ...attr }) => {
 	console.log(midiNote);
 	if (keyColor == "white") {
 		return (
-			<div className="bg-white border border-black px-12  text-black " {...attr}>
-				{midiNote}
+			<div className="bg-white border border-black px-12  text-black h-5" {...attr}>
+				{/* {midiNote} */}
 			</div>
 		);
 	} else {
 		return (
-			<div className=" bg-black border border-black px-12  text-white" {...attr}>
-				{midiNote}
+			<div className=" bg-black border border-black px-12  text-white h-5" {...attr}>
+				{/* {midiNote} */}
 			</div>
 		);
 	}
--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -5,6 +5,9 @@ import { KEYS, OCTAVE_STRUCTURE, OCTAVE_RANGES } from "../constants/constants";
 
 const Piano = () => {
 	const synth = useRef(null);
+
+	const isClicked = useRef(false);
+
 	//TODO const changecolor = "gray-700"
 
 	// TODO useRef to set boolean true or false when mouse is down or up
@@ -14,18 +17,39 @@ const Piano = () => {
 		synth.current = new Tone.Synth().toDestination();
 	}, []);
 
-	const onMouseDown = (e, n) => {
-		const note = Tone.Frequency(n, "midi").toNote();
+	const handleMouseDown = (e, n) => {
 		const now = Tone.now();
-		// trigger the attack immediately
+		var note = Tone.Frequency(n, "midi").toNote();
+
 		synth?.current?.triggerAttack(note, now);
+
+		isClicked.current = true;
 	};
 
-	const onMouseUp = () => {
+	const handleMouseUp = () => {
 		synth?.current?.triggerRelease();
+		isClicked.current = false;
 	};
+
+	const handleMouseEnter = (e, n) => {
+		if (isClicked.current) {
+			const now = Tone.now();
+			var note = Tone.Frequency(n, "midi").toNote();
+			synth?.current?.triggerAttack(note, now);
+		}
+	};
+
+	const handleMouseLeave = (outofdiv = false) => {
+		if (isClicked.current) {
+			synth?.current?.triggerRelease();
+		}
+	};
+
 	return (
-		<div className=" grid grid-cols-1 w-fit ">
+		<div
+			className=" grid grid-cols-1 w-fit "
+			onMouseLeave={handleMouseUp}
+		>
 			{/*h-[500px] overflow-scroll */}
 			{OCTAVE_RANGES.map((octave) =>
 				OCTAVE_STRUCTURE.map((key, index) => (
@@ -33,8 +57,14 @@ const Piano = () => {
 						key={index}
 						keyColor={KEYS[key]}
 						midiNote={octave[0] + index}
-						onMouseDown={(e) => onMouseDown(e, octave[0] + index)}
-						onMouseUp={(e) => onMouseUp(e, octave[0] + index)}
+						onMouseDown={(e) =>
+							handleMouseDown(e, octave[0] + index)
+						}
+						onMouseUp={(e) => handleMouseUp(e, octave[0] + index)}
+						onMouseEnter={(e) =>
+							handleMouseEnter(e, octave[0] + index)
+						}
+						onMouseLeave={handleMouseLeave}
 					/>
 				)).reverse()
 			).reverse()}
