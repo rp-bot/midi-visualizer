451f454f3b5dbac52269b20cae9ad6b63d4add7a
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2023-03-04 10:10:39 -0500

    -p

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Box = ({ beatnum, keynum, index, ...attr }) => {
+	const [noteStyle, setNoteStyle] = useState("");
+	const noteStatus = useRef(false);
+	const noteToggle = () => {
+		noteStatus.current = !noteStatus;
+		if (noteStatus) {
+			setNoteStyle("bg-blue-500 border-transparent");
+		} else {
+			setNoteStyle("");
+		}
+	};
+
 	return (
 		<div
-			className={`col-span-1 row-span-1 col-start-${beatnum} bg-gray-200  border border-gray-600 h-5 `}
+			className={`col-span-1 row-span-1 col-start-${beatnum} bg-gray-200  border border-gray-600 h-5 ${noteStyle} `}
+			onClick={noteToggle}
 			{...attr}
 		></div>
 	);
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -12,18 +12,22 @@ import {
 // 88 keys * 16
 
 const Grid = () => {
-	const [note, setNote] = useState(false);
+	// Hook to handle the click of the squares
+	// const [note, setNote] = useState(false);
 
-	const handleClick = () => {
-		if (!note) {
-			setNote(!note);
-			console.log(note);
-		} else {
-			console.log("already clicked");
-		}
-	};
+	// Stores the amount of notes per row. This will help us change the grid-cols
+	const notesPerRow = 0;
+
+	// const handleClick = () => {
+	// 	if (!note) {
+	// 		setNote(!note);
+	// 		// console.log(note);
+	// 	} else {
+	// 		setNote(!note);
+	// 	}
+	// };
 	return (
-		<div className="bg-gray-800  grid grid-rows-84 grid-cols-16  ">
+		<div className={`bg-gray-800  grid grid-rows-84 grid-cols-16  `}>
 			{KEYS_RANGE.map((keynum) =>
 				STAFF.map((beatnum, index) => {
 					return (
@@ -32,7 +36,7 @@ const Grid = () => {
 								key={index}
 								beatnum={beatnum}
 								keynum={keynum}
-								onClick={handleClick}
+								
 							/>
 						</div>
 					);
